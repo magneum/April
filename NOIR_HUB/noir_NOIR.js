57a075332df7969439d6beec52c5d2a41250dbf6
hypevoidsoul <hypevoidsoul@gmail.com>
2021-09-17 18:20:10 +0100

    🦋NOIR🦋==noir comd reworked

--- NOIR_HUB/noir_NOIR.js
+++ NOIR_HUB/noir_NOIR.js
@@ -4,8 +4,8 @@ const { PREFIX } = require("../NOIR_SYSTEM/noir_env");
 
 
 const NOIRED = `
-=• **🦋NOIR🦋** is a Discord Music player Bot with YouTube Support and is available on both Discord and Telegram.
-=• use ${PREFIX}help to learn how to use **🦋NOIR🦋**
+=• **🦋NOIR🦋** is a Discord Music player Bot with YouTube Support and is available on both Discord and Telegram.\n\n
+=• use ${PREFIX}help to learn how to use **🦋NOIR🦋**\n\n
 =• **🦋NOIR🦋** has been licensed under GNU General Public License 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗦𝗼𝘂𝗹 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗟𝗮𝗯 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝘀`
 
 
