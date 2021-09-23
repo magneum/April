ec97db32972f63d61d8c1e38691c2bc4bbeaa464
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-09-23 17:29:02 +0100

    🦋ɴᴏɪʀ🎧ᴘʟᴀʏᴇʀ🦋 ᴀᴜᴛᴏ ᴄʟᴇᴀɴᴇʀ ᴄᴏᴅᴇᴅ+ᴅᴇꜱɪɢɴ+ᴇᴍʙᴇᴅꜱ

--- noirhub/noir_ɴᴏɪʀᴇʟᴍx.js
+++ noirhub/noir_ɴᴏɪʀᴇʟᴍx.js
@@ -27,9 +27,9 @@ module.exports = {
     execute(message) {
         let NOIREMBED = new MessageEmbed()
             .setDescription(`
-=• **🦋ɴᴏɪʀ🎧ᴘʟᴀʏᴇʀ🦋** is a Discord Music player Bot with YouTube Support and is available on both Discord and Telegram.\n\n
+=• **ɴᴏɪʀ🎧ᴘʟᴀʏᴇʀ** is a Discord Music player Bot with YouTube Support and is available on both Discord and Telegram.\n\n
 =• use ${ʙᴏᴛꜰɪx}help to learn how to use **🦋ɴᴏɪʀ🎧ᴘʟᴀʏᴇʀ🦋**\n\n
-=• **🦋ɴᴏɪʀ🎧ᴘʟᴀʏᴇʀ🦋** has been licensed under GNU General Public License 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗦𝗼𝘂𝗹 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗟𝗮𝗯 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝘀`)
+=• **ɴᴏɪʀ🎧ᴘʟᴀʏᴇʀ** has been licensed under GNU General Public License 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗦𝗼𝘂𝗹 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗟𝗮𝗯 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝘀`)
             .setColor(`#32CD32`)
             .setAuthor(`🦋ɴᴏɪʀ🎧ᴘʟᴀʏᴇʀ🦋`)
             .setImage(`https://telegra.ph/file/3766d80c69f488d850173.jpg`)
