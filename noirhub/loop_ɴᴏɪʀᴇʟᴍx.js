977f82e79e968d34a7a26ae7fee1f06147707972
hypevoidsoul <hypevoidsoul@gmail.com>
2021-09-24 18:57:34 +0100

    🦋ɴᴏɪʀ🎧ᴘʟᴀʏᴇʀ🦋.js Modules reworked

--- noirhub/loop_ɴᴏɪʀᴇʟᴍx.js
+++ noirhub/loop_ɴᴏɪʀᴇʟᴍx.js
@@ -12,17 +12,6 @@
 const { canModifyQueue } = require("../noirtem/noir_env");
 const { ʙᴏᴛꜰɪx, ɴᴏɪʀᴄʟᴇᴀɴᴇʀ } = require("../noirtem/noir_env");
 const { MessageEmbed } = require("discord.js");
-// =============================================================================================================================
-// GNU GENERAL PUBLIC LICENSE
-// Version 3, 29 June 2007
-// Copyright (C) 2007 Free Software Foundation
-// Everyone is permitted to 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 verbatim copies
-// of this license document, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
-// noir🎧player
-// Discord Music YouTube player
-// has been licensed under GNU General Public License
-// 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗦𝗼𝘂𝗹 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗟𝗮𝗯 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝘀
-// =============================================================================================================================
 module.exports = {
   name: "loop",
   execute(message) {
