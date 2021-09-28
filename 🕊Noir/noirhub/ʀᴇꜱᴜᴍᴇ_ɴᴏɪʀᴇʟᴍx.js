a73a5448122c9ca3c0ef5da6407ce0c706eb4a7b
hypevoidsoul <hypevoidsoul@gmail.com>
2021-09-28 13:39:12 +0100

    🤍Noir-v13 + Bug Fixes + New Features  + Ui design

--- 🕊Noir/noirhub/ʀᴇꜱᴜᴍᴇ_ɴᴏɪʀᴇʟᴍx.js
+++ 🕊Noir/noirhub/ʀᴇꜱᴜᴍᴇ_ɴᴏɪʀᴇʟᴍx.js
@@ -1,178 +1,183 @@
 try {
-  const {
-    ʙᴏᴛꜰɪx,
-    ɴᴏɪʀᴄʟᴇᴀɴᴇʀ,
-    canModifyQueue,
-  } = require("../noirtem/noir_env");
-  const { MessageEmbed } = require("../ᴋʟᴀᴡᴠᴏɪᴅ/src");
-  // =============================================================================================================================
-  // GNU GENERAL PUBLIC LICENSE
-  // Version 3, 29 June 2007
-  // Copyright (C) 2007 Free Software Foundation
-  // Everyone is permitted to 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 verbatim copies
-  // of this license document, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
-  // 🤍Noir
-  // Discord Music YouTube player
-  // has been licensed under GNU General Public License
-  // 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗦𝗼𝘂𝗹 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗟𝗮𝗯 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝘀
-  // =============================================================================================================================
-  module.exports = {
-    name: "resume",
-    cooldown: 3,
-    // ==================================================================
-    // ================>  🎶Noir🤍PLΛYΣЯ by HypeVoidSoul <================
-    // ==================================================================
-    execute(message) {
-      if (
-        message.content.startsWith(ʙᴏᴛꜰɪx + "resume") &&
-        message.channel.name !== "🤍noir"
-      ) {
-        message.react("❌");
-        message.react("🔥");
-        const embedfactor = new MessageEmbed()
-          .setColor("#E0D268")
-          .setTitle("⚠️Warning⚠️")
-          .setAuthor(`•> 🤍Noir by HypeVoidSoul`)
-          .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
-          .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
-          .setDescription(
-            `
+const {
+ʙᴏᴛꜰɪx,
+ɴᴏɪʀᴄʟᴇᴀɴᴇʀ,
+canModifyQueue,
+} = require("../noirtem/noir_env");
+const { MessageEmbed } = require("../ᴋʟᴀᴡᴠᴏɪᴅ/src");
+// =============================================================================================================================
+// GNU GENERAL PUBLIC LICENSE
+// Version 3, 29 June 2007
+// Copyright (C) 2007 Free Software Foundation
+// Everyone is permitted to 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 verbatim copies
+// of this license document, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
+// 🤍Noir
+// Discord Music YouTube player
+// has been licensed under GNU General Public License
+// 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗦𝗼𝘂𝗹 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗟𝗮𝗯 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝘀
+// =============================================================================================================================
+module.exports = {
+name: "resume",
+cooldown: 3,
+
+
+
+
+execute(message) {
+if (
+message.content.startsWith(ʙᴏᴛꜰɪx + "resume") &&
+message.channel.name !== "🤍noir"
+) {
+message.react("❌");
+message.react("🔥");
+const embedfactor = new MessageEmbed()
+.setColor("#E0D268")
+.setTitle("⚠️Warning⚠️")
+.setAuthor(`•> 🤍Noir by HypeVoidSoul`)
+.setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+.setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
+.setDescription(
+`
 **User:** ${message.author}
 =========:radio_button:=========
 
 **Channel:** \`🤍Noir\`
 *Please use 👆🏻 channel for any* **ɴᴏɪʀ** *commands.*`
-          );
-        message.channel.send(embedfactor).catch(console.error);
-        // .then((message) => {
-        // message.delete({
-        // timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
-        // });
-        // });
-        return;
-      }
-      // ==================================================================
-      // ================>  🎶Noir🤍PLΛYΣЯ by HypeVoidSoul <================
-      // ==================================================================
-      if (
-        message.content.startsWith(ʙᴏᴛꜰɪx + "resume") &&
-        message.channel.name === "🤍noir"
-      ) {
-        const queue = message.client.queue.get(message.guild.id);
-        if (!queue) {
-          const embedresume1 = new MessageEmbed()
-            .setColor("#E0D268")
-            .setTitle("⚠️Warning⚠️")
-            .setAuthor(`•> 🤍Noir by HypeVoidSoul`)
-            .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
-            .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
-            .setDescription(
-              `
+);
+message.channel.send(embedfactor).catch(console.error);
+// .then((message) => {
+// message.delete({
+// timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
+// });
+// });
+return;
+}
+
+
+
+
+if (
+message.content.startsWith(ʙᴏᴛꜰɪx + "resume") &&
+message.channel.name === "🤍noir"
+) {
+const queue = message.client.queue.get(message.guild.id);
+if (!queue) {
+const embedresume1 = new MessageEmbed()
+.setColor("#E0D268")
+.setTitle("⚠️Warning⚠️")
+.setAuthor(`•> 🤍Noir by HypeVoidSoul`)
+.setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+.setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
+.setDescription(
+`
 **User:** ${message.author}
 =========:radio_button:=========
 
 is not playing anymusic yet.....`
-            );
-          message.react("❌");
-          message.react("🔥");
-          message.channel.send(embedresume1).catch(console.error);
-          // .then((message) => {
-          // message.delete({
-          // timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
-          // });
-          // });
-          return;
-        }
-        // ==================================================================
-        // ================>  🎶Noir🤍PLΛYΣЯ by HypeVoidSoul <================
-        // ==================================================================
-        if (!canModifyQueue(message.member)) {
-          const embedresume2 = new MessageEmbed()
-            .setColor("#E0D268")
-            .setTitle("⚠️Warning⚠️")
-            .setAuthor(`•> 🤍Noir by HypeVoidSoul`)
-            .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
-            .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
-            .setDescription(
-              `
+);
+message.react("❌");
+message.react("🔥");
+message.channel.send(embedresume1).catch(console.error);
+// .then((message) => {
+// message.delete({
+// timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
+// });
+// });
+return;
+}
+
+
+
+
+if (!canModifyQueue(message.member)) {
+const embedresume2 = new MessageEmbed()
+.setColor("#E0D268")
+.setTitle("⚠️Warning⚠️")
+.setAuthor(`•> 🤍Noir by HypeVoidSoul`)
+.setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+.setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
+.setDescription(
+`
 **User:** ${message.author}
 =========:radio_button:=========
 
 is not playing anymusic yet.....`
-            );
-          message.react("❌");
-          message.react("🔥");
-          message.channel.send(embedresume2).catch(console.error);
-          // .then((message) => {
-          // message.delete({
-          // timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
-          // });
-          // });
-          return;
-        }
-        // ==================================================================
-        // ================>  🎶Noir🤍PLΛYΣЯ by HypeVoidSoul <================
-        // ==================================================================
-        if (!queue.playing) {
-          queue.playing = true;
-          queue.connection.dispatcher.resume();
-          message.react("✅");
-          message.react("🍧");
-          queue.textChannel
-            .send(
-              new MessageEmbed()
-                .setColor("#6272a4")
-                .setAuthor(`•> 🤍Noir by HypeVoidSoul`)
-                .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
-                .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
-                .setDescription(`**User:** ${message.author}
+);
+message.react("❌");
+message.react("🔥");
+message.channel.send(embedresume2).catch(console.error);
+// .then((message) => {
+// message.delete({
+// timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
+// });
+// });
+return;
+}
+
+
+
+
+if (!queue.playing) {
+queue.playing = true;
+queue.connection.dispatcher.resume();
+message.react("✅");
+message.react("🍧");
+queue.textChannel
+.send(
+new MessageEmbed()
+.setColor("#6272a4")
+.setAuthor(`•> 🤍Noir by HypeVoidSoul`)
+.setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+.setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
+.setDescription(`**User:** ${message.author}
 =========:radio_button:=========
 
 ▶ Resumed the music!`)
-            )
-            .catch(console.error);
-          // .then((message) => {
-          // message.delete({
-          // timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
-          // });
-          // });
-          return;
-        }
-        // ==================================================================
-        // ================>  🎶Noir🤍PLΛYΣЯ by HypeVoidSoul <================
-        // ==================================================================
-        const embedresume3 = new MessageEmbed()
-          .setColor("#E0D268")
-          .setTitle("⚠️Warning⚠️")
-          .setAuthor(`•> 🤍Noir by HypeVoidSoul`)
-          .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
-          .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
-          .setDescription(
-            `
+)
+.catch(console.error);
+// .then((message) => {
+// message.delete({
+// timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
+// });
+// });
+return;
+}
+
+
+
+
+const embedresume3 = new MessageEmbed()
+.setColor("#E0D268")
+.setTitle("⚠️Warning⚠️")
+.setAuthor(`•> 🤍Noir by HypeVoidSoul`)
+.setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+.setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
+.setDescription(
+`
 **User:** ${message.author}
 =========:radio_button:=========
 
 The queue is not paused.`
-          );
-        message.react("❌");
-        message.react("🔥");
-        message.channel.send(embedresume3).catch(console.error);
-        // .then((message) => {
-        // message.delete({
-        // timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
-        // });
-        // });
-        return;
-      }
-    },
-  };
+);
+message.react("❌");
+message.react("🔥");
+message.channel.send(embedresume3).catch(console.error);
+// .then((message) => {
+// message.delete({
+// timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
+// });
+// });
+return;
+}
+},
+};
 } catch (ErrorNoir) {
-  const ErrorInNoir = new MessageEmbed()
-    .setColor("#DB4434")
-    .setTitle("🔺ERROR CAUGHT🔻")
-    .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
-    .setThumbnail("https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg")
-    .setDescription(`
+const ErrorInNoir = new MessageEmbed()
+.setColor("#DB4434")
+.setTitle("🔺ERROR CAUGHT🔻")
+.setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
+.setThumbnail("https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg")
+.setDescription(`
 **Noir** has encountered an error.
 
 Please either report to **https://discord.gg/ucPpXWFK**  in discord channel
@@ -182,6 +187,6 @@ Report to **@hypevoids** in telegram group
 
 **🔺Error Caught🔻**
 *${ErrorNoir}*`);
-  message.channel.send(ErrorInNoir);
-  console.error(ErrorNoir);
+message.channel.send(ErrorInNoir);
+console.error(ErrorNoir);
 }
--- 🕊Noir/noirhub/ʟᴏᴏᴘ_ɴᴏɪʀᴇʟᴍx.js
+++ 🕊Noir/noirhub/ʟᴏᴏᴘ_ɴᴏɪʀᴇʟᴍx.js
@@ -1,147 +1,151 @@
 try {
-  const { canModifyQueue } = require("../noirtem/noir_env");
-  const { ʙᴏᴛꜰɪx, ɴᴏɪʀᴄʟᴇᴀɴᴇʀ } = require("../noirtem/noir_env");
-  const { MessageEmbed } = require("../ᴋʟᴀᴡᴠᴏɪᴅ/src");
-  // =============================================================================================================================
-  // GNU GENERAL PUBLIC LICENSE
-  // Version 3, 29 June 2007
-  // Copyright (C) 2007 Free Software Foundation
-  // Everyone is permitted to 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 verbatim copies
-  // of this license document, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
-  // 🤍Noir
-  // Discord Music YouTube player
-  // has been licensed under GNU General Public License
-  // 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗦𝗼𝘂𝗹 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗟𝗮𝗯 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝘀
-  // =============================================================================================================================
-  module.exports = {
-    name: "loop",
-    cooldown: 3,
-    // ==================================================================
-    // ================>  🎶Noir🤍PLΛYΣЯ by HypeVoidSoul <================
-    // ==================================================================
-    execute(message) {
-      if (
-        message.content.startsWith(ʙᴏᴛꜰɪx + "loop") &&
-        message.channel.name !== "🤍noir"
-      ) {
-        message.react("❌");
-        message.react("🔥");
-        const embedfactor = new MessageEmbed()
-          .setColor("#E0D268")
-          .setTitle("⚠️Warning⚠️")
-          .setAuthor(`•> 🤍Noir by HypeVoidSoul`)
-          .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
-          .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
-          .setDescription(
-            `
+const { canModifyQueue } = require("../noirtem/noir_env");
+const { ʙᴏᴛꜰɪx, ɴᴏɪʀᴄʟᴇᴀɴᴇʀ } = require("../noirtem/noir_env");
+const { MessageEmbed } = require("../ᴋʟᴀᴡᴠᴏɪᴅ/src");
+// =============================================================================================================================
+// GNU GENERAL PUBLIC LICENSE
+// Version 3, 29 June 2007
+// Copyright (C) 2007 Free Software Foundation
+// Everyone is permitted to 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 verbatim copies
+// of this license document, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
+// 🤍Noir
+// Discord Music YouTube player
+// has been licensed under GNU General Public License
+// 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗦𝗼𝘂𝗹 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗟𝗮𝗯 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝘀
+// =============================================================================================================================
+module.exports = {
+name: "loop",
+cooldown: 3,
+
+
+
+
+execute(message) {
+if (
+message.content.startsWith(ʙᴏᴛꜰɪx + "loop") &&
+message.channel.name !== "🤍noir"
+) {
+message.react("❌");
+message.react("🔥");
+const embedfactor = new MessageEmbed()
+.setColor("#E0D268")
+.setTitle("⚠️Warning⚠️")
+.setAuthor(`•> 🤍Noir by HypeVoidSoul`)
+.setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+.setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
+.setDescription(
+`
 **User:** ${message.author}
 =========:radio_button:=========
 
 **Channel:** \`🤍Noir\`
 *Please use 👆🏻 channel for any* **ɴᴏɪʀ** *commands.*`
-          );
-        message.channel.send(embedfactor).catch(console.error);
-        // .then((message) => {
-        //   message.delete({
-        //     timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
-        //   });
-        // });
-        return;
-      }
-      // ==================================================================
-      // ================>  🎶Noir🤍PLΛYΣЯ by HypeVoidSoul <================
-      // ==================================================================
-      if (
-        message.content.startsWith(ʙᴏᴛꜰɪx + "loop") &&
-        message.channel.name === "🤍noir"
-      ) {
-        const queue = message.client.queue.get(message.guild.id);
-        if (!queue) {
-          const embedloopno1 = new MessageEmbed()
-            .setColor("#E0D268")
-            .setTitle("⚠️Warning⚠️")
-            .setAuthor(`•> 🤍Noir by HypeVoidSoul`)
-            .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
-            .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
-            .setDescription(`
+);
+message.channel.send(embedfactor).catch(console.error);
+// .then((message) => {
+//   message.delete({
+//     timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
+//   });
+// });
+return;
+}
+
+
+
+
+if (
+message.content.startsWith(ʙᴏᴛꜰɪx + "loop") &&
+message.channel.name === "🤍noir"
+) {
+const queue = message.client.queue.get(message.guild.id);
+if (!queue) {
+const embedloopno1 = new MessageEmbed()
+.setColor("#E0D268")
+.setTitle("⚠️Warning⚠️")
+.setAuthor(`•> 🤍Noir by HypeVoidSoul`)
+.setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+.setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
+.setDescription(`
 **User:** ${message.author}
 =========:radio_button:=========
 
 Noir is not playing anymusic yet.....`);
-          message.react("❌");
-          message.react("🔥");
-          message.channel.send(embedloopno1).catch(console.error);
-          // .then((message) => {
-          //   message.delete({
-          //     timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
-          //   });
-          // });
-          return;
-        }
-        // ==================================================================
-        // ================>  🎶Noir🤍PLΛYΣЯ by HypeVoidSoul <================
-        // ==================================================================
-        if (!canModifyQueue(message.member)) {
-          const embedloopno2 = new MessageEmbed()
-            .setColor("#E0D268")
-            .setTitle("⚠️Warning⚠️")
-            .setAuthor(`•> 🤍Noir by HypeVoidSoul`)
-            .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
-            .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
-            .setDescription(`
+message.react("❌");
+message.react("🔥");
+message.channel.send(embedloopno1).catch(console.error);
+// .then((message) => {
+//   message.delete({
+//     timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
+//   });
+// });
+return;
+}
+
+
+
+
+if (!canModifyQueue(message.member)) {
+const embedloopno2 = new MessageEmbed()
+.setColor("#E0D268")
+.setTitle("⚠️Warning⚠️")
+.setAuthor(`•> 🤍Noir by HypeVoidSoul`)
+.setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+.setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
+.setDescription(`
 **User:** ${message.author}
 =========:radio_button:=========
 
 *You need to* **join** *a voice channel first!*`);
-          message.react("❌");
-          message.react("🔥");
-          message.channel.send(embedloopno2).catch(console.error);
-          // .then((message) => {
-          //   message.delete({
-          //     timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
-          //   });
-          // });
-          return;
-        }
-        // ==================================================================
-        // ================>  🎶Noir🤍PLΛYΣЯ by HypeVoidSoul <================
-        // ==================================================================
-        queue.loop = !queue.loop;
-        message.react("✅");
-        message.react("🍧");
-        queue.textChannel
-          .send(
-            new MessageEmbed()
-              .setColor("#6272a4")
-              .setAuthor(`•> 🤍Noir by HypeVoidSoul`)
-              .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
-              .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
-              .setDescription(`**User:** ${message.author}
+message.react("❌");
+message.react("🔥");
+message.channel.send(embedloopno2).catch(console.error);
+// .then((message) => {
+//   message.delete({
+//     timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
+//   });
+// });
+return;
+}
+
+
+
+
+queue.loop = !queue.loop;
+message.react("✅");
+message.react("🍧");
+queue.textChannel
+.send(
+new MessageEmbed()
+.setColor("#6272a4")
+.setAuthor(`•> 🤍Noir by HypeVoidSoul`)
+.setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+.setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
+.setDescription(`**User:** ${message.author}
 =========:radio_button:=========
 
 Queue Loop is now turned ${
-              queue.loop
-                ? message.channel.send("On")
-                : message.channel.send("Off")
-            }**`)
-          )
-          .catch(console.error);
-        // .then((message) => {
-        // message.delete({
-        // timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
-        // });
-        // });
-        return;
-      }
-    },
-  };
+queue.loop
+? message.channel.send("On")
+: message.channel.send("Off")
+}**`)
+)
+.catch(console.error);
+// .then((message) => {
+// message.delete({
+// timeout: `${ɴᴏɪʀᴄʟᴇᴀɴᴇʀ}`,
+// });
+// });
+return;
+}
+},
+};
 } catch (ErrorNoir) {
-  const ErrorInNoir = new MessageEmbed()
-    .setColor("#DB4434")
-    .setTitle("🔺ERROR CAUGHT🔻")
-    .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
-    .setThumbnail("https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg")
-    .setDescription(`
+const ErrorInNoir = new MessageEmbed()
+.setColor("#DB4434")
+.setTitle("🔺ERROR CAUGHT🔻")
+.setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: 𝙶𝙽𝚄(𝙲)𝟸𝟶𝟸𝟷 𝙷𝚢𝚙𝚎𝚅𝚘𝚒𝚍𝙻𝚊𝚋")
+.setThumbnail("https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg")
+.setDescription(`
 **Noir** has encountered an error.
 
 Please either report to **https://discord.gg/ucPpXWFK**  in discord channel
@@ -151,6 +155,6 @@ Report to **@hypevoids** in telegram group
 
 **🔺Error Caught🔻**
 *${ErrorNoir}*`);
-  message.channel.send(ErrorInNoir);
-  console.error(ErrorNoir);
+message.channel.send(ErrorInNoir);
+console.error(ErrorNoir);
 }
