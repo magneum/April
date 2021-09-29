0500d4f8215eb0faddc4136c1b97803674183d6b
HypeVoidSoul <hypevoidsoul@gmail.com>
2021-09-29 05:26:00 +0100

    🦋𝗡𝗢𝗜𝗥🦋 v14 upcoming
    🦋𝗡𝗢𝗜𝗥🦋 v14 upcoming

---
+++ 🦋𝗡𝗢𝗜𝗥🦋/ռօɨʀɦʊɮ/ɴᴏɪʀ_ɴᴏɪʀᴇʟᴍx.js
@@ -0,0 +1,113 @@
+try {
+  const { MessageEmbed } = require("../ӄʀǟӄɨռʐʟǟɮ/src");
+  const { ռօɨʀʄɨӼ, ռօɨʀքʊʀɢɛʀ } = require("../noirtem/noir_env");
+  // =============================================================================================================================
+  // GNU GENERAL PUBLIC LICENSE
+  // Version 3, 29 June 2007
+  // 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗞𝗿𝗮𝗸𝗶𝗻𝘇 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗟𝗮𝗯 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗕𝗼𝘁
+  // =============================================================================================================================
+  module.exports = {
+    name: "noir",
+    cooldown: 3,
+  // =============================================================================================================================
+  // GNU GENERAL PUBLIC LICENSE
+  // Version 3, 29 June 2007
+  // 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗞𝗿𝗮𝗸𝗶𝗻𝘇 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗟𝗮𝗯 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗕𝗼𝘁
+  // =============================================================================================================================
+    execute(message) {
+      if (
+        message.content.startsWith(ռօɨʀʄɨӼ + "noir") &&
+        message.channel.name === "💜noir"
+      ) {
+        let NoirEMBED = new MessageEmbed()
+          .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: GNU(C)2021 Krakinz & KrakinzLab")
+          .setDescription(
+            `**User:** ${message.author}
+=========⚜️=========
+
+•> **Noir** is a Discord Music player Bot with YouTube Support and is available on both Discord and Telegram.\n
+•> **Noir** has been licensed under GNU General Public License 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗞𝗿𝗮𝗸𝗶𝗻𝘇 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗟𝗮𝗯 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗕𝗼𝘁\n
+•> use **${ռօɨʀʄɨӼ}help** to learn how to use **Noir**
+`
+          )
+          .setColor("#6272a4")
+          .setAuthor(`𝐍𝐨𝐢𝐫💜𝐛𝐲🔱𝐊𝐫𝐚𝐤𝐢𝐧𝐳`)
+          .setImage(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`);
+
+        message.react("✅");
+        message.react("🍧");
+        message.author.send(
+          new MessageEmbed()
+            .setColor("#6272a4")
+            .setDescription(`**👍 Sent from <#${message.channel.id}>**`)
+        );
+
+        message.author.send(NoirEMBED).catch(console.error);
+        message.channel
+          .send(
+            new MessageEmbed()
+              .setColor("#6272a4")
+              .setAuthor(`𝐍𝐨𝐢𝐫💜𝐛𝐲🔱𝐊𝐫𝐚𝐤𝐢𝐧𝐳`)
+              .setImage(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+              .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+              .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: GNU(C)2021 Krakinz & KrakinzLab")
+              .setDescription(`**👍 ${message.author} 
+=========⚜️=========
+
+Check your \`direct messages\` 
+for extra info on *Noir* **
+`)
+          )
+          .then((message) => {
+            message.delete({
+              timeout: `${ռօɨʀքʊʀɢɛʀ}`,
+            });
+          });
+        return;
+      }
+      if (
+        message.content.startsWith(ռօɨʀʄɨӼ + "noir") &&
+        message.channel.name !== "💜noir"
+      ) {
+        let NoirEMBED = new MessageEmbed()
+          .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: GNU(C)2021 Krakinz & KrakinzLab")
+          .setDescription(
+            `**User:** ${message.author}
+=========⚜️=========
+
+•> **Noir** is a Discord Music player Bot with YouTube Support and is available on both Discord and Telegram.\n
+•> **Noir** has been licensed under GNU General Public License 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗞𝗿𝗮𝗸𝗶𝗻𝘇 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗟𝗮𝗯 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗕𝗼𝘁\n
+•> use **${ռօɨʀʄɨӼ}help** to learn how to use **Noir**
+`
+          )
+          .setColor("#6272a4")
+          .setAuthor(`𝐍𝐨𝐢𝐫💜𝐛𝐲🔱𝐊𝐫𝐚𝐤𝐢𝐧𝐳`)
+          .setImage(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`);
+
+        message.react("✅");
+        message.react("🍧");
+        message.channel.send(NoirEMBED).catch(console.error);
+        return;
+      }
+    },
+  };
+} catch (ErrorNoir) {
+  message.channel.send(
+    new MessageEmbed()
+      .setColor("#DB4434")
+      .setTitle("🔺ERROR CAUGHT🔻")
+      .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: GNU(C)2021 Krakinz & KrakinzLab")
+      .setThumbnail("https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg")
+      .setDescription(`
+**Noir** has encountered an error.
+
+Please either report to 🔰**https://discord.gg/ucPpXWFK**  in discord channel
+or
+Report to 🔰**@Krakns** in telegram group
+
+
+**🔺Error Caught🔻**
+*${ErrorNoir}*`)
+  );
+  console.error(ErrorNoir);
+}
---
+++ 🦋𝗡𝗢𝗜𝗥🦋/ռօɨʀɦʊɮ/ꜱᴋɪᴘᴛᴏ_ɴᴏɪʀᴇʟᴍx.js
@@ -0,0 +1,223 @@
+try {
+  const {
+    canModifyQueue,
+    ռօɨʀʄɨӼ,
+    ռօɨʀքʊʀɢɛʀ,
+  } = require("../noirtem/noir_env");
+  const { MessageEmbed } = require("../ӄʀǟӄɨռʐʟǟɮ/src");
+  // =============================================================================================================================
+  // GNU GENERAL PUBLIC LICENSE
+  // Version 3, 29 June 2007
+  // 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗞𝗿𝗮𝗸𝗶𝗻𝘇 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗟𝗮𝗯 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗕𝗼𝘁
+  // =============================================================================================================================
+  module.exports = {
+    name: "skipto",
+    cooldown: 3,
+    // =============================================================================================================================
+    // GNU GENERAL PUBLIC LICENSE
+    // Version 3, 29 June 2007
+    // 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗞𝗿𝗮𝗸𝗶𝗻𝘇 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗟𝗮𝗯 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗕𝗼𝘁
+    // =============================================================================================================================
+    execute(message, args) {
+      if (
+        message.content.startsWith(ռօɨʀʄɨӼ + "skipto") &&
+        message.channel.name !== "💜noir"
+      ) {
+        message.react("❌");
+        message.react("🔥");
+        message.channel
+          .send(
+            new MessageEmbed()
+              .setColor("#E0D268")
+              .setTitle("⚠️Warning⚠️")
+              .setAuthor(`𝐍𝐨𝐢𝐫💜𝐛𝐲🔱𝐊𝐫𝐚𝐤𝐢𝐧𝐳`)
+              .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+              .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: GNU(C)2021 Krakinz & KrakinzLab")
+              .setDescription(
+                `
+**User:** ${message.author}
+=========⚜️=========
+
+**Category:** \`🔱Krakinz\`
+**Channel:** \`💜Noir\`
+⚠️Please use 👆🏻 for any **ɴᴏɪʀ** commands.`
+              )
+          )
+          .catch(console.error);
+        // .then((message) => {
+        // message.delete({
+        // timeout: `${ռօɨʀքʊʀɢɛʀ}`,
+        // });
+        // });
+        return;
+      }
+
+      if (
+        message.content.startsWith(ռօɨʀʄɨӼ + "skipto") &&
+        message.channel.name === "💜noir"
+      ) {
+        if (!args.length || isNaN(args[0])) {
+          message.react("❌");
+          message.react("🔥");
+          message.channel
+            .send(
+              new MessageEmbed()
+                .setColor("#E0D268")
+                .setTitle("⚠️Warning⚠️")
+                .setAuthor(`𝐍𝐨𝐢𝐫💜𝐛𝐲🔱𝐊𝐫𝐚𝐤𝐢𝐧𝐳`)
+                .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+                .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: GNU(C)2021 Krakinz & KrakinzLab")
+                .setDescription(
+                  `
+**User:** ${message.author}
+=========⚜️=========
+
+⚓️**usage:**${ռօɨʀʄɨӼ}{name} <Queue Number>`
+                )
+            )
+            .catch(console.error);
+          // .then((message) => {
+          // message.delete({
+          // timeout: `${ռօɨʀքʊʀɢɛʀ}`,
+          // });
+          // });
+          return;
+        }
+
+        const queue = message.client.queue.get(message.guild.id);
+        if (!queue) {
+          message.react("❌");
+          message.react("🔥");
+          message.channel
+            .send(
+              new MessageEmbed()
+                .setColor("#E0D268")
+                .setTitle("⚠️Warning⚠️")
+                .setAuthor(`𝐍𝐨𝐢𝐫💜𝐛𝐲🔱𝐊𝐫𝐚𝐤𝐢𝐧𝐳`)
+                .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+                .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: GNU(C)2021 Krakinz & KrakinzLab")
+                .setDescription(
+                  `
+**User:** ${message.author}
+=========⚜️=========
+
+There is nothing playing that I could skip for you.`
+                )
+            )
+            .catch(console.error);
+          // .then((message) => {
+          // message.delete({
+          // timeout: `${ռօɨʀքʊʀɢɛʀ}`,
+          // });
+          // });
+          return;
+        }
+
+        if (!canModifyQueue(message.member)) {
+          message.react("❌");
+          message.react("🔥");
+          message.channel
+            .send(
+              new MessageEmbed()
+                .setColor("#E0D268")
+                .setTitle("⚠️Warning⚠️")
+                .setAuthor(`𝐍𝐨𝐢𝐫💜𝐛𝐲🔱𝐊𝐫𝐚𝐤𝐢𝐧𝐳`)
+                .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+                .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: GNU(C)2021 Krakinz & KrakinzLab")
+                .setDescription(
+                  `
+**User:** ${message.author}
+=========⚜️=========
+
+*You need to* **join** *a voice channel first!*`
+                )
+            )
+            .catch(console.error);
+          // .then((message) => {
+          // message.delete({
+          // timeout: `${ռօɨʀքʊʀɢɛʀ}`,
+          // });
+          // });
+          return;
+        }
+
+        if (args[0] > queue.songs.length) {
+          message.react("❌");
+          message.react("🔥");
+          message.channel
+            .send(
+              new MessageEmbed()
+                .setColor("#E0D268")
+                .setTitle("⚠️Warning⚠️")
+                .setAuthor(`𝐍𝐨𝐢𝐫💜𝐛𝐲🔱𝐊𝐫𝐚𝐤𝐢𝐧𝐳`)
+                .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+                .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: GNU(C)2021 Krakinz & KrakinzLab")
+                .setDescription(
+                  `
+**User:** ${message.author}
+=========⚜️=========
+
+There is nothing playing that I could skip for you.`
+                )
+            )
+            .catch(console.error);
+          // .then((message) => {
+          // message.delete({
+          // timeout: `${ռօɨʀքʊʀɢɛʀ}`,
+          // });
+          // });
+          return;
+        }
+
+        queue.playing = true;
+        if (queue.loop) {
+          for (let i = 0; i < args[0] - 2; i++) {
+            queue.songs.push(queue.songs.shift());
+          }
+        } else {
+          queue.songs = queue.songs.slice(args[0] - 2);
+        }
+        queue.connection.dispatcher.end();
+        message.react("✅");
+        message.react("🍧");
+        queue.textChannel
+          .send(
+            new MessageEmbed()
+              .setColor("#6272a4")
+              .setAuthor(`𝐍𝐨𝐢𝐫💜𝐛𝐲🔱𝐊𝐫𝐚𝐤𝐢𝐧𝐳`)
+              .setThumbnail(`https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg`)
+              .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: GNU(C)2021 Krakinz & KrakinzLab")
+              .setDescription(`**User:** ${message.author}
+=========⚜️=========
+
+⏺ stopped the music!`)
+          )
+          .catch(console.error);
+        // .then((message) => {
+        // message.delete({
+        // timeout: `${ռօɨʀքʊʀɢɛʀ}`,
+        // });
+        // });
+      }
+    },
+  };
+} catch (ErrorNoir) {
+  message.channel.send(
+    new MessageEmbed()
+      .setColor("#DB4434")
+      .setTitle("🔺ERROR CAUGHT🔻")
+      .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: GNU(C)2021 Krakinz & KrakinzLab")
+      .setThumbnail("https://i.postimg.cc/fTKfYqx0/Noir-Player.jpg")
+      .setDescription(`
+**Noir** has encountered an error.
+
+Please either report to 🔰**https://discord.gg/ucPpXWFK**  in discord channel
+or
+Report to 🔰**@Krakns** in telegram group
+
+
+**🔺Error Caught🔻**
+*${ErrorNoir}*`)
+  );
+  console.error(ErrorNoir);
+}
