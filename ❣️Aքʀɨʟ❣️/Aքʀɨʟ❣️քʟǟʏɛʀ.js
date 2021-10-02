22cf55eedc9e8d1696b3c751168500aed5a72ba5
krakinz <krakinzkon@gmail.com>
2021-10-02 05:37:14 +0100

    Aքʀɨʟ❣️քʟǟʏɛʀ_by🔱ӄʀǟӄɨռʐʟǟɮ™️ -v14

--- ❣️Aքʀɨʟ❣️/Aքʀɨʟ❣️քʟǟʏɛʀ.js
+++ ❣️Aքʀɨʟ❣️/Aքʀɨʟ❣️քʟǟʏɛʀ.js
@@ -45,21 +45,23 @@ try {
       type: "WATCHING",
     });
     console.log(`
-~~~~~~~~~~~~~~~~~~~~~~~>  𝐀𝐩𝐫𝐢𝐥❣️by🔱KrakinzLab™️ <~~~~~~~~~~~~~~~~~~~~~~~
+♥️--------------- 𝐀𝐩𝐫𝐢𝐥❣️by🔱KrakinzLab™️ ---------------♥️
 🔱Bot-Name:~~> ${Aքʀɨʟքʟǟʏɛʀ.user.username}
 🔱Bot-Os:~~> ${os.platform().toUpperCase()}
 🔱Bot-Port:~~> ${PORT}
-~~~~~~~~~~~~~~~~~~~~~~~>  𝐀𝐩𝐫𝐢𝐥❣️by🔱KrakinzLab™️ <~~~~~~~~~~~~~~~~~~~~~~~
+
+♥️--------------- 𝐀𝐩𝐫𝐢𝐥❣️by🔱KrakinzLab™️ ---------------♥️
 GNU GENERAL PUBLIC LICENSE
 Version 3, 29 June 2007
 Copyright (C) 2007 Free Software Foundation
 Everyone is permitted to 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 verbatim copies
 of this license document, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
-𝐀𝐩𝐫𝐢𝐥❣️by🔱KrakinzLab™️
+♥️ 𝐀𝐩𝐫𝐢𝐥❣️by🔱KrakinzLab™️ ♥️
 Discord Music YouTube player
 has been licensed under GNU General Public License
 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗞𝗿𝗮𝗸𝗶𝗻𝘇 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗟𝗮𝗯 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗕𝗼𝘁
-~~~~~~~~~~~~~~~~~~~~~~~>  𝐀𝐩𝐫𝐢𝐥❣️by🔱KrakinzLab™️ <~~~~~~~~~~~~~~~~~~~~~~~`);
+
+♥️--------------- 𝐀𝐩𝐫𝐢𝐥❣️by🔱KrakinzLab™️ ---------------♥️`);
   });
 } catch (ErrorApril) {
   console.error(ErrorApril);
